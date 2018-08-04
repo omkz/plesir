afaa7c1cfdd21f108f09064c9db0ce11eaa6c916
Kurnia Muhamad <omenkzz@gmail.com>
2018-08-04 21:15:02 +0700

    added user

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,11 @@ module Plesir
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework  nil #to skip test framework
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
   end
 end
